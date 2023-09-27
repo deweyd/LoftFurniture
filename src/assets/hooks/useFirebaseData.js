dc5132b13ed976d7bfbe47b0a46992453e191626
deweyd <37833987+deweyd@users.noreply.github.com>
2023-09-27 16:37:16 +0300

    Update useFirebaseData.js

--- src/assets/hooks/useFirebaseData.js
+++ src/assets/hooks/useFirebaseData.js
@@ -7,14 +7,14 @@ const useFirebaseData = (collectionName) => {
 
     useEffect(() => {
         const firebaseConfig = {
-            apiKey: "AIzaSyCjrFpvD5cmmQT28VsaCnjvXKlxtp6lEEA",
-            authDomain: "newproject-9cd70.firebaseapp.com",
-            databaseURL: "https://newproject-9cd70-default-rtdb.firebaseio.com",
-            projectId: "newproject-9cd70",
-            storageBucket: "newproject-9cd70.appspot.com",
-            messagingSenderId: "229652802780",
-            appId: "1:229652802780:web:054be34a9a7494ea8e1819",
-            measurementId: "G-6YCSJ2GZ0N"
+            apiKey: process.env.REACT_APP_API_KEY,
+            authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+            databaseURL: process.env.REACT_APP_DATA_BASE_URL,
+            projectId: process.env.REACT_APP_PROJECT_ID,
+            storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+            messagingSenderId: process.env.REACT_APP_MESSAGGING_SENDER_ID,
+            appId: process.env.REACT_APP_APP_ID,
+            measurementId: process.env.REACT_APP_MEASUREMENT_ID
         };
 
         if (!firebase.apps.length) {
